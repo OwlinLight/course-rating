07919c5e16fd4a7b1db7a51827d1836284355aed
Kevin-Haller137 <haller.137@buckeyemail.osu.edu>
2022-12-10 21:38:09 -0500

    small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+
   end
 
   # POST /users or /users.json
@@ -55,15 +56,9 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.find(params[:id])
+    redirect_to main_path
+
   end
 
   # DELETE /users/1 or /users/1.json
