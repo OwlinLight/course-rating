06b95198c66956bd96a0acaeb93bfcb81939736e
Kevin-Haller137 <haller.137@buckeyemail.osu.edu>
2022-12-10 20:07:20 -0500

    Big updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
+
+  def joinClass
+    @student = current_user
+    @klass.users << @student
+
+    redirect_to @klass
+  end
+
+  
   def signup
     @user = User.new
   end
